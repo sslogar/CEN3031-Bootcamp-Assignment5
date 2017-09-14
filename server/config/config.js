2c8eab1bf17178989436b444ece231b918085979
Stephen <sslogar@ufl.edu>
2017-09-14 15:48:51 -0400

    Added Config Files

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://sslogar:thisisafakepassword@ds119064.mlab.com:19064/sslogarcen3031', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
